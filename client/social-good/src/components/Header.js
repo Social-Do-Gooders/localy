d987e862e546b7ff62db07836d02164933a8f2a9
david-osas <azamegbeosarumense@gmail.com>
2020-12-05 19:46:29 +0100

    Add react router dom

--- client/social-good/src/components/Header.js
+++ client/social-good/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 function Header(){
@@ -6,7 +7,7 @@ function Header(){
   return(
   <div className='app-container'>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Logo</a>
+      <Link className="navbar-brand">Logo</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -14,13 +15,13 @@ function Header(){
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">Businesses</a>
+            <Link className="nav-link" href="#">Businesses</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Meetups</a>
+            <Link className="nav-link" href="#">Meetups</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">News</a>
+            <Link className="nav-link" href="#">News</Link>
           </li>
         </ul>
       </div>
