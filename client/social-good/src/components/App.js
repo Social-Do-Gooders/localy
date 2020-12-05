83e2e0b77a5f7d5d4674a7127515be11acae2af1
david-osas <azamegbeosarumense@gmail.com>
2020-12-05 14:20:44 +0100

    Add carousel component

---
+++ client/social-good/src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles.css';
+import Carousel from './Carousel';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Carousel />
+    </div>    
+  );
+}
+
+export default App;
---
+++ client/social-good/src/components/Carousel.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import placeholder1 from '../images/carousel/placeholder1.jpg';
+import placeholder2 from '../images/carousel/placeholder2.jpg';
+import placeholder3 from '../images/carousel/placeholder3.jpg';
+
+
+function Carousel(){
+
+  return (
+    <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={placeholder1} className="carousel-img" alt="placeholder"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={placeholder2} className="carousel-img" alt="placeholder"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={placeholder3} className="carousel-img" alt="placeholder"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+  );
+}
+
+
+export default Carousel;
