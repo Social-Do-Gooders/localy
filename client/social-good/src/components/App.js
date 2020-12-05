59a1cd3c3f1c618fe5f9b499ea752e46b07703e1
david-osas <azamegbeosarumense@gmail.com>
2020-12-05 19:32:13 +0100

    Add initial header

--- client/social-good/src/components/App.js
+++ client/social-good/src/components/App.js
@@ -1,10 +1,12 @@
 import Home from './home/Home';
+import Header from './Header';
 import '../css/general.css';
 import '../css/component.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Home />
     </div>
   );
