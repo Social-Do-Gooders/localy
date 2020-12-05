d68912cc58a5bd5a98d98c268fcdb44397b9aede
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-05 09:50:12 -0500

    Add Some Initial Data

--- server/code/resources/user.py
+++ server/code/resources/user.py
@@ -1,7 +1,17 @@
 from flask_restful import Resource
 
+users = [
+    {'first_name': 'Andrea', 'last_name': 'Jones', 'email_address': 'andrea@jones.com'},
+    {'first_name': 'Bob', 'last_name': 'Smith', 'email_address': 'bob@smith.com'},
+    {'first_name': 'Catherine', 'last_name': 'Jones', 'email_address': 'catherine@jones.com'},
+    {'first_name': 'David', 'last_name': 'Murphy', 'email_address': 'david@murphy.com'},
+
+]
+
+
+
 class UserList(Resource):
 
     # GET method
     def get(self):
-        return {'users': ['Andrea', 'Bob', 'Catherine', 'David']}
+        return {'users': users}
