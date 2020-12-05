05f3f834b64446cddf36c8237f45e030a5d20c54
david-osas <azamegbeosarumense@gmail.com>
2020-12-05 15:05:49 +0100

    Add starter code to feature header component

--- client/social-good/src/components/home/FeatureHeader.js
+++ client/social-good/src/components/home/FeatureHeader.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import placeholder1 from '../../images/carousel/placeholder1.jpg';
+import placeholder2 from '../../images/carousel/placeholder2.jpg';
+import placeholder3 from '../../images/carousel/placeholder3.jpg';
 
-function FeatureHeader({text}){
+function FeatureHeader({text, feature}){
+  let imgSrc;
+
+  switch(feature){
+    case 'fund':
+    imgSrc = placeholder1;
+    break;
+
+    case 'schedule':
+    imgSrc = placeholder2;
+    break;
+
+    case 'news':
+    imgSrc = placeholder3;
+    break;
+
+    default:
+    imgSrc = placeholder1;
+  }
 
   return(
-    <div>
+    <div className='feature-header'>
+      <p className='feature-header-icon'><img src={imgSrc}/></p>
       <h1 className='feature-text'>{text}</h1>
     </div>
   );
--- client/social-good/src/components/home/HomeFeatures.js
+++ client/social-good/src/components/home/HomeFeatures.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import FeatureHeader from './FeatureHeader';
-import FeatureIntro from './FeatureIntro';
+import FeatureIntroGrid from './FeatureIntroGrid';
 
+function HomeFeatures() {
+  let features = [
+    {
+      header: 'First header',
+      feature: 'fund'
+    }, {
+      header: 'Second header',
+      feature: 'schedule'
+    }, {
+      header: 'Third header',
+      feature: 'news'
+    }
+  ]
 
-function HomeFeatures(){
-
-  return(
-
-  );
+  return (<div>
+    {
+      featuress.map((item,index) => <div>
+        <FeatureHeader key={index} text={item.header} feature={item.feature}/>
+        <FeatureIntroGrid key={index} feature={item.feature}/>
+      </div>)
+    }
+  </div>);
 }
-
 
 export default HomeFeatures;
