b55b6c96c864ce2aa1dd615765d73881151cfbc9
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-06 15:02:47 -0500

    Generate User Data with Faker

--- server/code/resources/user.py
+++ server/code/resources/user.py
@@ -1,12 +1,33 @@
 from flask_restful import Resource
+from faker import Faker
+import random
 
-users = [
-    {'first_name': 'Andrea', 'last_name': 'Jones', 'email_address': 'andrea@jones.com'},
-    {'first_name': 'Bob', 'last_name': 'Smith', 'email_address': 'bob@smith.com'},
-    {'first_name': 'Catherine', 'last_name': 'Jones', 'email_address': 'catherine@jones.com'},
-    {'first_name': 'David', 'last_name': 'Murphy', 'email_address': 'david@murphy.com'},
+#### GENERATE FAKE DATA
 
-]
+# initialize Faker
+fake = Faker()
+
+# initialize list
+users = []
+
+# generate users
+for _ in range(10):
+
+    # generate user
+    user = {}
+    user['id'] = fake.uuid4()
+    user['first_name'] = fake.first_name()
+    user['last_name'] = fake.last_name()
+    user['email_address'] = fake.email()
+    user['password'] = fake.password()
+    user['address'] = fake.address()
+    #user['date_of_birth'] = fake.date_of_birth()
+    user['organizations_following'] = [fake.company() for _ in range(random.randint(1,3))]
+    user['users_following'] = [fake.name() for _ in range(random.randint(1,3))]
+    user['services'] = [fake.job() for _ in range(random.randint(1,3))]
+
+    # append to list
+    users.append(user)
 
 
 
