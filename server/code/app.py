b8ad0535bf7cd6148075286f5bc7c1b86f5781b0
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-06 15:57:34 -0500

    Create Funds Resource with Fake Data and Connect with App

--- server/code/app.py
+++ server/code/app.py
@@ -8,6 +8,7 @@ from flask_restful import Api
 from resources.user import Users
 from resources.organization import Organizations
 from resources.article import Articles
+from resources.fund import Funds
 
 #### APP SETUP
 app = Flask(__name__)
@@ -24,6 +25,7 @@ def index():
 api.add_resource(Users, '/users')
 api.add_resource(Organizations, '/organizations')
 api.add_resource(Articles, '/articles')
+api.add_resource(Funds, '/funds')
 
 
 # Run app
