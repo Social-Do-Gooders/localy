6551434070fcb617a57eb8e501f5709dedccf9d4
david-osas <azamegbeosarumense@gmail.com>
2020-12-06 23:57:49 +0100

    Add onclick handlers to home feature buttons

--- client/social-good/src/components/home/FeatureIntroGrid.js
+++ client/social-good/src/components/home/FeatureIntroGrid.js
@@ -1,28 +1,39 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import placeholder1 from '../../images/carousel/placeholder1.jpg';
 import placeholder2 from '../../images/carousel/placeholder2.jpg';
 import placeholder3 from '../../images/carousel/placeholder3.jpg';
 
 
 function FeatureIntroGrid({feature, altText, btnText}){
+  let history = useHistory();
+
   let data = [1,2,3,4,5,6];
-  let imgSrc;
+  let imgSrc, path;
 
   switch(feature){
     case 'fund':
     imgSrc = placeholder1;
+    path = 'business';
     break;
 
     case 'schedule':
     imgSrc = placeholder2;
+    path = 'meetups';
     break;
 
     case 'news':
     imgSrc = placeholder3;
+    path = 'news';
     break;
 
     default:
     imgSrc = placeholder1;
+    path = 'business';
+  }
+
+  function handleOnClick(){
+    history.push(path);
   }
 
   return (
@@ -35,7 +46,7 @@ function FeatureIntroGrid({feature, altText, btnText}){
             <p className='intro-grid-img-content'>Name of object</p>
           </div>)}
         </div>
-        <button type="button" className="intro-btn">{btnText}</button>
+        <button type="button" className="intro-btn" onClick={handleOnClick}>{btnText}</button>
       </div>
     </div>
   );
