18c3f30efa0304dbb99916df1377a6b0eca60dff
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-06 15:25:23 -0500

    Add About Section to Organization Data

--- server/code/resources/organization.py
+++ server/code/resources/organization.py
@@ -1,5 +1,29 @@
 from flask_restful import Resource
 from faker import Faker
+import random
+
+#### GENERATE FAKE DATA
+
+# initialize Faker
+fake = Faker()
+
+# initialize list
+organizations = []
+
+# generate organizations
+for _ in range(10):
+
+    # generate organization
+    organization = {}
+    organization['id'] = fake.uuid4()
+    organization['name'] = fake.company()
+    organization['owner'] = fake.name()
+    organization['address'] = fake.address()
+    organization['about'] = fake.paragraph()
+    organization['followers'] = [fake.name() for _ in range(random.randint(1, 5))]
+
+    # append to list
+    organizations.append(organization)
 
 #### CLASSES
 
@@ -7,4 +31,4 @@ class Organizations(Resource):
      
      # GET method
      def get(self):
-         return {'organizations': []}
+         return {'organizations': organizations}
