2c7a8391e41df44486837fb817cc115c92221246
david-osas <azamegbeosarumense@gmail.com>
2020-12-07 14:14:37 +0100

    Add footer-link css class

--- client/social-good/src/components/utils/Footer.js
+++ client/social-good/src/components/utils/Footer.js
@@ -14,10 +14,10 @@ function Footer(){
         <FavoriteIcon className='footer-heart'/>
         &nbsp;
         by
-        <a href={osas} className='text-white' target='_blank' rel='noreferrer'> <em>Osas</em> </a>,
-        <a href={yashika} className='text-white' target='_blank' rel='noreferrer'> <em>Yashika</em> </a>,
-        <a href={nkosi} className='text-white' target='_blank' rel='noreferrer'> <em>Nkosi</em> </a>,
-        <a href={aneesh} className='text-white' target='_blank' rel='noreferrer'> <em>Aneesh</em> </a>
+        <a href={osas} className='footer-link' target='_blank' rel='noreferrer'> <em>Osas</em> </a>,
+        <a href={yashika} className='footer-link' target='_blank' rel='noreferrer'> <em>Yashika</em> </a>,
+        <a href={nkosi} className='footer-link' target='_blank' rel='noreferrer'> <em>Nkosi</em> </a>,
+        <a href={aneesh} className='footer-link' target='_blank' rel='noreferrer'> <em>Aneesh</em> </a>
       </p>
     </footer>
   )
