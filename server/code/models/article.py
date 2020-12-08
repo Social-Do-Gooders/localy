aa6b3e360cbfe38e826482ab83275183c9598a3e
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-08 17:46:31 -0500

    Add Article Model

---
+++ server/code/models/article.py
@@ -0,0 +1,16 @@
+from db import db
+
+class ArticleModel(db.Document):
+
+    title = db.StringField(required=True)
+    content = db.StringField(required=True)
+    author = db.StringField(required=True)
+    date = db.DateTimeField()
+   
+    meta = {'collection': 'articles'}
+
+    def save_to_db(self):
+        self.save()
+
+    def delete_from_db(self):
+        self.delete()
