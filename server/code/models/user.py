3fad3ae1de296532304819125a3faea643d213ff
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-08 17:34:53 -0500

    Add Methods to User Model

--- server/code/models/user.py
+++ server/code/models/user.py
@@ -4,7 +4,7 @@ class UserModel(db.Document):
 
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
-    email_address = db.StringField(required=True)
+    email_address = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
     organizations_following = db.ListField(default=[])
     users_following = db.ListField(default=[])
@@ -14,3 +14,13 @@ class UserModel(db.Document):
 
     def json(self):
         return self.to_json()
+
+    def save_to_db(self):
+        self.save()
+
+    def delete_from_db(self):
+        self.delete()
+
+    @classmethod
+    def find_by_email(cls, email_address):
+        return UserModel.objects(email_address=email_address).first()
