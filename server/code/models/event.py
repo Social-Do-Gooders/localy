ed2ace66a02502c369283dbdb0aef5a3bea18e80
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-08 17:44:49 -0500

    Add Event Model

---
+++ server/code/models/event.py
@@ -0,0 +1,18 @@
+from db import db
+
+class EventModel(db.Document):
+
+    name = db.StringField(required=True)
+    organizer = db.StringField(required=True)
+    location = db.StringField(required=True)
+    about = db.StringField(required=True)
+    attendees = db.ListField(default=[])
+    date = db.DateTimeField()
+  
+    meta = {'collection': 'events'}
+
+    def save_to_db(self):
+        self.save()
+
+    def delete_from_db(self):
+        self.delete()
