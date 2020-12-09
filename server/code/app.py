0d3c8e3423c60eabd38c58b43acf2d877326ed68
nkosi-tauro <nkosilati23@yahoo.com>
2020-12-09 13:31:00 +0200

    added new route for news

--- server/code/app.py
+++ server/code/app.py
@@ -1,5 +1,5 @@
 #### IMPORT
-
+import os
 # python imports
 from flask import Flask
 from flask_restful import Api
@@ -11,6 +11,13 @@ from resources.article import Articles
 from resources.fund import Funds
 from resources.event import Events
 
+# importing requests, json package 
+import requests  
+import json  
+
+
+#### API 
+SECRET_API_KEY = os.getenv("REACT_APP_NEWS_API_KEY")
 #### APP SETUP
 app = Flask(__name__)
 
@@ -29,6 +36,41 @@ api.add_resource(Articles, '/articles')
 api.add_resource(Funds, '/funds')
 api.add_resource(Events, '/events')
 
+  
+# Fetch Articles 
+def newsJSON(newsType): 
+      
+    #news api 
+    main_url = "https://newsapi.org/v2/everything?q="+newsType+"&sortBy=publishedAt&apiKey="+SECRET_API_KEY
+    
+    articles = []
+    # fetching data in json format 
+    data = requests.get(main_url).json()
+    # return an array of objects
+    for allArticles in data['articles'] :
+        news = {
+            'newsTitle' : allArticles['title'],
+            'newsUrl' : allArticles['url'],
+            'imgUrl': allArticles['urlToImage']
+        } 
+        articles.append(news)
+    
+
+    return articles
+
+@app.route('server/news/<query>',method='POST')
+def news(query):
+    print('query =',str(query),sep=' ')
+    return newsJSON(query)
+
+#Incase of no query provided
+@app.route('server/news/',method='POST')
+def emptyquery():
+    print('empty query')
+    return json.dumps([{
+        'status' : 'No response',
+        'error' : 'Empty query'
+    }])
 
 # Run app
 if __name__ == "__main__":
