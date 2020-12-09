b11960a46c502d54780b621e70b3d914e33567d7
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-09 12:13:19 -0500

    Add Method to User Model to Follow Organization

--- server/code/models/user.py
+++ server/code/models/user.py
@@ -1,4 +1,6 @@
 from db import db
+from models.organization import OrganizationModel
+from models.service import ServiceModel
 
 class UserModel(db.Document):
 
@@ -6,9 +8,9 @@ class UserModel(db.Document):
     last_name = db.StringField(required=True)
     email_address = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
-    organizations_following = db.ListField(default=[])
-    users_following = db.ListField(default=[])
-    services = db.ListField(default=[])
+    organizations_following = db.ListField(db.ReferenceField(OrganizationModel), default=[])
+    users_following = db.ListField(db.ReferenceField("self"), default=[])
+    services = db.ListField(db.ReferenceField(ServiceModel), default=[])
   
     meta = {'collection': 'users'}
 
@@ -20,6 +22,12 @@ class UserModel(db.Document):
 
     def delete_from_db(self):
         self.delete()
+
+    def add_service(self, service_object):
+        self.services.append(service_object)
+
+    def follow_organization(self, organization_object):
+        self.organizations_following.append(organization_object)
 
     @classmethod
     def find_by_email(cls, email_address):
