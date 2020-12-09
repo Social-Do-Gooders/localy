b7040b8970e86cab088348ef2bdf69ea94e6ef7e
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-09 11:12:14 -0500

    Add Article Type Column and Add JSON Method to Article Model

--- server/code/models/article.py
+++ server/code/models/article.py
@@ -3,12 +3,15 @@ from db import db
 class ArticleModel(db.Document):
 
     title = db.StringField(required=True)
-    content = db.StringField(required=True)
-    author = db.StringField(required=True)
+    content = db.StringField()
+    author = db.StringField()
     date = db.DateTimeField()
-    article_type = db.StringField()
+    article_type = db.StringField(default='other', choices=['business news', 'tech news', 'science news', 'other'])
    
     meta = {'collection': 'articles'}
+
+    def json(self):
+        return self.to_json()
 
     def save_to_db(self):
         self.save()
