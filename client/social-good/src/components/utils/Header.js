12997071403b7b9dcbebf6151bd1079a716637af
david-osas <azamegbeosarumense@gmail.com>
2020-12-10 00:29:03 +0100

    Add Logo

--- client/social-good/src/components/utils/Header.js
+++ client/social-good/src/components/utils/Header.js
@@ -6,7 +6,7 @@ function Header(){
   return(
   <div className='header-item'>
     <nav className="navbar navbar-expand-lg navbar-dark">
-      <Link className="navbar-brand text-white" to='/'>Logo</Link>
+      <Link className="navbar-brand text-white" to='/'><span className='logo'>Localy</span></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
