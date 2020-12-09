7057b50e092fac8bbf90bb87292051d3094327d3
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-09 11:04:28 -0500

    Add Event Type Column to Event Model

--- server/code/models/event.py
+++ server/code/models/event.py
@@ -3,14 +3,17 @@ from db import db
 class EventModel(db.Document):
 
     name = db.StringField(required=True)
-    organizer = db.StringField(required=True)
-    location = db.StringField(required=True)
-    about = db.StringField(required=True)
+    organizer = db.StringField()
+    location = db.StringField()
+    about = db.StringField()
     attendees = db.ListField(default=[])
     date = db.DateTimeField()
-    event_type = db.StringField()
+    event_type = db.StringField(default='other', choices=['tech', 'science', 'finance', 'artisan skills', 'health', 'education', 'art', 'other'])
   
     meta = {'collection': 'events'}
+
+    def json(self):
+        return self.to_json()
 
     def save_to_db(self):
         self.save()
